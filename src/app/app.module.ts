474da5ebb36974e50e2ef338bfb464fb5154559c
Felix He <felixhe.ski@outlook.com>
2020-08-21 22:28:33 -0400

    Added Agora video calling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { NgxAgoraModule, AgoraConfig } from 'ngx-agora';
+
+const agoraConfig: AgoraConfig = {
+  AppID: 'd83866456f8948c39c5fca40707f337c',
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxAgoraModule.forRoot(agoraConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
